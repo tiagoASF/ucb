c494464a3785947a7845312bdfa06c3301bb52c3
Tiago Fialho <tiago.chem@gmail.com>
2020-04-06 07:54:45 -0300

    exercicio de composicao aluno|turma

---
+++ aula03/atividade09/src/Atividade09.java
@@ -0,0 +1,11 @@
+/*
+Uma escola deseja fazer um cadastro dos seus alunos. Um aluno possui nome, matrícula e situação,
+que pode ser regular ou bolsista. O usuário informará quantos alunos existem em uma turma.
+Você receberá os dados de todos os alunos e, na sequência, você deve apresentar todos os alunos informados,
+a quantidade de alunos regulares que existe e a quantidade de alunos bolsistas.
+ */
+
+
+
+public class Atividade09 {
+}
