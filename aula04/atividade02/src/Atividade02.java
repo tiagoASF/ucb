5de318afc2971ff77eb0c589fe9f1a0bd0d3817b
Tiago Fialho <tiago.chem@gmail.com>
2020-04-08 16:18:17 -0300

    enunciado da questão - manipulação de strings

---
+++ aula04/atividade02/src/Atividade02.java
@@ -0,0 +1,13 @@
+/*
+Manipular Strings é uma atividade corriqueira de um programador. Crie um programa que fará a leitura de uma String do
+usuário e você deverá apresentar os seguintes resultados:
+
+- A string informada em caixa alta.
+- A string informada em caixa baixa.
+- A quantidade de caracteres que a string contém.
+- Se a frase digitada é igual ou não a "Eu estou estudando Java".
+- A quantidade de palavras que a frase possui.
+ */
+
+public class Atividade02 {
+}
