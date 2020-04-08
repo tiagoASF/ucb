97b909d36e4ebca1aa2fd17223a6bf6bf6ceb565
Tiago Fialho <tiago.chem@gmail.com>
2020-04-08 16:13:16 -0300

    exercicio concluído - uso de classes abstrata

--- aula04/atividade04/src/Atividade04.java
+++ aula04/atividade04/src/Atividade04.java
@@ -9,16 +9,53 @@ quantidade de professores que são Mestre. Fique atento onde cada método e atri
 
 import java.util.Scanner;
 import java.util.Locale;
+import java.util.List;
+import java.util.ArrayList;
+
 import entities.*;
+import entities.enums.Titulacao;
 
 public class Atividade04 {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        int opcao;
+        List<Professor> corpoDocente = new ArrayList<>();
 
+        do {
 
+            System.out.println("INFORME OS DADOS DO DOCENTE");
+            System.out.print("Nome: ");
+            String nome = sc.nextLine();
+            System.out.print("CPF: ");
+            String cpf = sc.next();
+            System.out.print("Salário: ");
+            double salario = sc.nextDouble();
+            System.out.print("Titulacao (graduado/mestre/doutor): ");
+            String titulacao = sc.next();
+            Titulacao titulo = Titulacao.valueOf(titulacao);
 
+            Professor professor = new Professor(nome, cpf, salario, titulo);
+            corpoDocente.add(professor);
 
+            System.out.print("Inserir professor? 1 - SIM, 0 - NÃO ");
+            opcao = sc.nextInt();
+            sc.nextLine();
+        } while (opcao == 1);
 
+        double impostoSindicalTotal = 0.0;
+        int numMestres = 0;
+        for (Professor p : corpoDocente) {
+            impostoSindicalTotal += Tributacao.impostoSindical(p);
+            if (p.getTitulo() == Titulacao.MESTRE) {
+                numMestres++;
+            }
+        }
+
+        System.out.printf("Imposto sindical arrecadado: R$ %.2f\n", impostoSindicalTotal);
+        System.out.println("Total de mestres: " + numMestres);
+
+        sc.close();
     }
+
 }
