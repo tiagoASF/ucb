30561ef48724a501815419a8dfb0bb46ba2a6b8b
Tiago Fialho <tiago.chem@gmail.com>
2020-04-08 18:47:47 -0300

    enunciado da questão - API String

---
+++ aula04/atividade09/src/Atividade09.java
@@ -0,0 +1,12 @@
+/*
+Receba do usuário uma frase e faça o que se pede baseado na API String:
+- A quantidade de letras 'A' que a frase tem.
+- O tamanho da frase (quantidade de caracteres).
+- A qualificação da frase (menor que 10 caracteres – PEQUENA, maior que 9 e menor que 29 – MEDIA, maior que 30 é GRANDE).
+- Apresente a frase informada em CAIXA ALTA.
+- A quantidade de palavras que a frase contém.
+- Valide para verificar se a String contém a palavra "Corrupção", pois se não houver deve ser solicitada novamente ao usuário.
+*/
+
+public class Atividade09 {
+}
