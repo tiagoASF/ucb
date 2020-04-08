0e704952397734b12360b1fd96ef08d193c7ed03
Tiago Fialho <tiago.chem@gmail.com>
2020-04-08 19:15:19 -0300

    item 1 respondido

--- aula04/atividade09/src/Atividade09.java
+++ aula04/atividade09/src/Atividade09.java
@@ -7,6 +7,18 @@ Receba do usuário uma frase e faça o que se pede baseado na API String:
 - A quantidade de palavras que a frase contém.
 - Valide para verificar se a String contém a palavra "Corrupção", pois se não houver deve ser solicitada novamente ao usuário.
 */
+import java.util.Scanner;
 
 public class Atividade09 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String fraseDoUsuario;
+
+        System.out.print("Digite uma frase qualquer: ");
+        fraseDoUsuario = sc.nextLine().toLowerCase();
+        
+        //Respostas
+        System.out.print("Frase em caixa alta: " + fraseDoUsuario.toUpperCase());
+
+    }
 }
